0d8b81cd080f8155db68b29efa735062e0d59fa8
Michael Gomoluch <mgomoluch1125@stu.d214.org>
2020-02-04 11:23:02 -0600

    got table view working and created a new class by the name of item

---
+++ ShoppingList/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Michael Gomoluch on 2/4/20.
+//  Copyright © 2020 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+        
+    }
+    
+}
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var newItemTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        
     }
     @IBAction func whenButtonPressed(_ sender: Any) {
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 6
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 }
 
